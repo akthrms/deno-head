b078a6dcf1137c3cc9835cf2377d49dd33f25bf5
akthrms <akt.hrms@gmail.com>
2023-05-10 21:20:48 +0900

    refactor: 関数単位でインポートする

--- cli.ts
+++ cli.ts
@@ -1,6 +1,6 @@
-import * as io from "https://deno.land/std@0.167.0/io/mod.ts";
+import { readLines } from "https://deno.land/std@0.167.0/io/mod.ts";
 import { Reader } from "https://deno.land/std@0.167.0/io/types.d.ts";
-import * as path from "https://deno.land/std@0.185.0/path/mod.ts";
+import { isAbsolute, join } from "https://deno.land/std@0.185.0/path/mod.ts";
 import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
 
 await new Command()
@@ -23,7 +23,7 @@ await new Command()
 
       try {
         reader = await Deno.open(
-          path.isAbsolute(file) ? file : path.join(Deno.cwd(), file)
+          isAbsolute(file) ? file : join(Deno.cwd(), file)
         );
       } catch {
         console.log(`failed to read file: ${file}`);
@@ -36,7 +36,7 @@ await new Command()
     const maxLines = lines ?? 10;
     let curLines = 0;
 
-    for await (const line of io.readLines(reader)) {
+    for await (const line of readLines(reader)) {
       if (curLines >= maxLines) {
         break;
       }
